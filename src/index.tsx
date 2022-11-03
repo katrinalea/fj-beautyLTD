8e40928418efd6d3a95eb679399ea07e7d03561d
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-03 17:07:46 +0000

    corrections

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./Header";
-import MainContent from "./MainContent";
+//import MainContent from "./MainContent";
 
-ReactDOM.render(<Footer />, document.getElementById("footer"));
+
 ReactDOM.render(<Header />, document.getElementById("header"));
-ReactDOM.render(<MainContent />, document.getElementById("maincontent"));
+//ReactDOM.render(<MainContent />, document.getElementById("maincontent"));
