c65007acdb57ddc97d58db6983addf788ed56e39
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-03 18:35:34 +0000

    upate

--- src/Header.tsx
+++ src/Header.tsx
@@ -6,15 +6,12 @@ function Header(): JSX.Element {
       <h2> Eyelash and Brow specialist </h2>
 
       <p>
-        <a href="https://www.instagram.com/freyajensenbeauty/" target="_blank">
+        <a href="https://www.instagram.com/freyajensenbeauty/">
           {" "}
           Follow our Instagram !{" "}
         </a>{" "}
         |
-        <a
-          href="https://www.google.co.uk/maps/place/Bebington,+Wirral/data=!4m2!3m1!1s0x487ad898b2a90b47:0x79ca4c8aa4bf5ba2"
-          target="_blank"
-        >
+        <a href="https://www.google.co.uk/maps/place/Bebington,+Wirral/data=!4m2!3m1!1s0x487ad898b2a90b47:0x79ca4c8aa4bf5ba2">
           {" "}
           Studio address{" "}
         </a>
