25ad1e4e686588cfc949d597c6ce0cb0b0fe1c9d
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-03 16:55:51 +0000

    starter

---
+++ src/Header.tsx
@@ -0,0 +1,18 @@
+
+function Header(): JSX.Element {
+    return (
+    <div>
+
+    <h1> Freya Jensen Beauty LTD</h1>
+
+    <h2> Eyelash and Brow specialist </h2>
+
+    <a href = "https://www.instagram.com/freyajensenbeauty/"> Follow our Instagram ! </a>
+    
+    <a href = "https://www.google.co.uk/maps/place/Bebington,+Wirral/data=!4m2!3m1!1s0x487ad898b2a90b47:0x79ca4c8aa4bf5ba2"> Studio address </a>
+    
+    </div>
+    )}
+
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import Header from "./Header";
+import MainContent from "./MainContent";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Footer />, document.getElementById("footer"));
+ReactDOM.render(<Header />, document.getElementById("header"));
+ReactDOM.render(<MainContent />, document.getElementById("maincontent"));
