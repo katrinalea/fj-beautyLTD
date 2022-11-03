70680588a96397c8a7ffe2628915ea269b0bca3b
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-03 18:16:38 +0000

    format

--- src/Header.tsx
+++ src/Header.tsx
@@ -4,13 +4,13 @@ function Header(): JSX.Element {
 
     <div className = "header">
 
-    <h1 className = "a"> Freya Jensen Beauty LTD</h1>
+    <h1> Freya Jensen Beauty LTD</h1>
 
     <h2> Eyelash and Brow specialist </h2>
 
-    <a href= "https://www.instagram.com/freyajensenbeauty/"> Follow our Instagram ! </a>
+    <p><a href= "https://www.instagram.com/freyajensenbeauty/" target = "_blank"> Follow our Instagram ! </a> |
     
-    <a href= "https://www.google.co.uk/maps/place/Bebington,+Wirral/data=!4m2!3m1!1s0x487ad898b2a90b47:0x79ca4c8aa4bf5ba2"> Studio address </a>
+    <a href= "https://www.google.co.uk/maps/place/Bebington,+Wirral/data=!4m2!3m1!1s0x487ad898b2a90b47:0x79ca4c8aa4bf5ba2" target = "_blank"> Studio address </a></p>
     
     </div>
     )}
