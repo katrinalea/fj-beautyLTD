527d03bd34ceffd63363364308e74bc69f1e98de
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-03 18:25:44 +0000

    review

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import Header from "./Header"
 import "./style.css"
+import Review from "./Review"
 
 function App (): JSX.Element{
     return(
+    <div>
     <div className = "a">
         <Header />
     </div>
-    
+    <div>
+       <Review />
+    </div>
+    </div>
     )
 }
 
---
+++ src/Review.tsx
@@ -0,0 +1,10 @@
+function Review (): JSX.Element{
+ return (
+    <div className = "review">
+    <input type="text"> Please leave me a review. </input>
+    <button>Submit</button>
+    </div>
+ )
+}
+
+export default Review;
