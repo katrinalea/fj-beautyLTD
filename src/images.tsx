207fd6e6e5db75876f08803d82aef1f748e7def4
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-04 14:40:53 +0000

    photo

--- src/images.tsx
+++ src/images.tsx
@@ -1,8 +1,8 @@
 function Image(): JSX.Element {
   return (
     <div className="image">
-      <img src="freya.jpeg" alt="" width="150" height="200" />
-    </div>
-  );
+      <img alt ="" src="https://www.google.com/imgres?imgurl=https%3A%2F%2Flookaside.fbsbx.com%2Flookaside%2Fcrawler%2Fmedia%2F%3Fmedia_id%3D402544168229389&imgrefurl=https%3A%2F%2Fwww.facebook.com%2FFreyaJensenBeauty%2F&tbnid=MWqWJuRrh3_FWM&vet=12ahUKEwjv-c6s4JT7AhUtQEEAHS_WBfkQMygBegQIARA8..i&docid=44pRoAHfIveQAM&w=982&h=1080&q=freya%20jensen%20beauty%20eyelashes&ved=2ahUKEwjv-c6s4JT7AhUtQEEAHS_WBfkQMygBegQIARA8" width = "150" height = "200" />
+   </div>
+   );
 }
 export default Image;
