f822e1889764a3bb4be9cbda502139e6d65590ae
Katrina <katrinalea.woolley@yahoo.co.uk>
2022-11-04 15:05:35 +0000

    format

--- src/images.tsx
+++ src/images.tsx
@@ -1,10 +1,8 @@
-
-
 function Image(): JSX.Element {
   return (
     <div className="image">
-      <img alt ="" src="freya.jpeg" width = "150" height = "200" />
-   </div>
-   );
+      <img alt="" src="freya.jpeg" width="150" height="200" />
+    </div>
+  );
 }
 export default Image;
